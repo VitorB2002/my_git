0a9408890c928c6ba80382b132c434dc08572abd
VitorB2002 <vitorb.santos2002@gmail.com>
2024-08-29 12:32:29 -0300

    feat: adicionado criação de documento .json com as informações do MR

--- main.py
+++ main.py
@@ -11,6 +11,16 @@ if __name__ == '__main__':
   project_id = int(input("Insira o id do projeto: "))
   state = input("Insira o estado do merge request (opened, closed, merged, all): ")
   username = input("Insira o nome do usuário: ")
+  user_mrs = []
   mrs = get_mrs(project_id, state, username)
 
-  print(mrs)
+  for mr in mrs:
+    user_mrs.append({
+      "id": mr["id"],
+      "reference": mr["reference"],
+      "title": mr["title"],
+      "description": mr["description"]
+    })
+
+  with open("./files/user_mrs.json", "w") as arquivo:
+    json.dump(user_mrs, arquivo, indent=4)
